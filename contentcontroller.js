e8050eb01262bf6d3003226764c3a714396a82d6
Josh Kuo <Josh.Kuo@dlinkcorp.com>
2019-06-16 11:28:59 +0800

    test

--- contentcontroller.js
+++ contentcontroller.js
@@ -39,18 +39,19 @@ app.directive('windowDetection', ['$window', function ($window) {
        restrict: 'A'     
     };
     function link(scope, element, attrs){
-       scope.width = $window.innerWidth;
+        scope.width = $window.innerWidth;
        
-           function onResize(){
-               console.log($window.innerWidth);
-               // uncomment for only fire when $window.innerWidth change   
-               if (scope.width !== $window.innerWidth)
-               {
-                   scope.width = $window.innerWidth;
-                   scope.$digest();
-               }
-           };
+        function onResize(){
+            console.log($window.innerWidth);
+            // uncomment for only fire when $window.innerWidth change   
+            if (scope.width !== $window.innerWidth)
+            {
+                scope.width = $window.innerWidth;
+                scope.$digest();
+            }
+            $scope.$apply();
+        }
 
-           angular.element($window).bind('resize', onResize);
+        angular.element($window).bind('resize', onResize);
     }    
 }]);
